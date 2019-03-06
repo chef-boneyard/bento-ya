42cd3f2c20c7ec7b10c408227cc2100b29d97f96
Tim Smith <tsmith@chef.io>
2019-03-06 11:40:43 -0800

    WIP: Conver the upload command to just shellout to vagrant
    There are multiple bugs in the vagrant API gem that make it a less than desirable way to get the files up to Vagrant Cloud. I've fixed a few of them and reported others, but we need to get something working here so this just rips that out and uses the command instead. It's slower and more error prone, but it gets us something that works. If that gem gets improvements sometime down the line we can revisit this.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/bento/upload.rb
+++ lib/bento/upload.rb
@@ -1,4 +1,5 @@
 require "bento/common"
+require 'json'
 
 class UploadRunner
   include Common
@@ -20,30 +21,68 @@ class UploadRunner
     banner("Uploads finished in #{duration(time.real)}.")
   end
 
-  def upload(md_file)
-    puts "Attempting to upload #{md_file}"
-    md = box_metadata(md_file)
-    box_desc = "a bento box for #{md['name']}"
-    box = vc_account.ensure_box(md["name"], {short_description: box_desc, is_private: private_box?(md["name"])})
-    box_ver = box.ensure_version(md["version"], File.read(md_file))
+  def box_cmd(action, md)
+    box_create_cmd = "vagrant cloud box #{action} bento/#{md['name']} -s 'A minimal test box for #{md['name']} created with Bento by Chef'"
+    cmd = Mixlib::ShellOut.new(box_create_cmd)
+    cmd.run_command
+    cmd
+  end
 
-    if builds_yml["slugs"].value?(box.name)
-      slug_desc = "a bento box for #{builds_yml['slugs'].key(box.name)}"
-      slug = vc_account.ensure_box(builds_yml["slugs"].key(box.name), {short_description: slug_desc, is_private: false})
-      slug_ver = slug.ensure_version(md["version"], File.read(md_file))
+  def version_cmd(action, md)
+    version_create_cmd = "vagrant cloud version #{action} bento/#{md['name']} #{md["version"]} -d 'A minimal test box for #{md['name']} created with Bento by Chef'"
+    cmd = Mixlib::ShellOut.new(version_create_cmd)
+    cmd.run_command
+    cmd
+  end
+
+  def upload_necessary?(boxname, version, provider)
+    begin
+      search_output = JSON.parse(Mixlib::ShellOut.new("vagrant cloud search bento/#{boxname} --json").run_command.stdout)
+    rescue JSON::ParserError => e
+      if e.to_s.match?(/No results found/)
+        return true # new box that's never been published
+      else
+        raise e
+      end
     end
 
-    md["providers"].each do |k, v|
-      provider = box_ver.ensure_provider(k, nil)
-      banner("Uploading #{box.name}/#{box_ver.version}/#{provider.name}...")
-      provider.upload_file("builds/#{v['file']}")
-      banner(provider.download_url.to_s)
-      next unless builds_yml["slugs"].value?(box.name)
+    box_entry = search_output.select{|entry| entry['name'] == "bento/#{boxname}" } # grab the exact box vs. fuzzy search results we get back
 
-      slug_provider = slug_ver.ensure_provider(k, nil)
-      banner("Uploading #{slug.name}/#{slug_ver.version}/#{slug_provider.name}...")
-      slug_provider.upload_file("builds/#{v['file']}")
-      banner(slug_provider.download_url.to_s)
+    return true unless box_entry[0]['version'] == version
+    return true unless box_entry[0]['providers'].include?(provider)
+    return false
+  end
+
+  def upload(md_file)
+    banner "Attempting to upload #{md_file}"
+    md = box_metadata(md_file)
+
+    box_cmd('update', md) if box_cmd('create', md).stderr.match?(/has already been taken/)
+    banner "bento/#{md['name']} box entry created"
+
+    version_cmd('update', md) if version_cmd('create', md).stderr.match?(/has already been taken/)
+    banner "bento/#{md['name']} version entry #{md["version"]} created"
+
+    #
+    # if builds_yml["slugs"].value?(box.name)
+    #   slug_desc = "A Bento box for #{builds_yml['slugs'].key(box.name)}"
+    #   slug = vc_account.ensure_box(builds_yml["slugs"].key(box.name), {short_description: slug_desc, is_private: false})
+    #   slug_ver = slug.ensure_version(md["version"], File.read(md_file))
+    # end
+
+    md["providers"].each do |k, v|
+      upload_necessary?(md['name'], md['version'], k)
+
+      banner("Starting upload of bento/#{md['name']} #{md['version']} #{k} from builds/#{v['file']}. This will take a while...")
+      upload = Mixlib::ShellOut.new("vagrant cloud publish bento/#{md['name']} #{md['version']} #{k} builds/#{v['file']} -fr").run_command
+      require 'pry'; binding.pry
+      #
+      # next unless builds_yml["slugs"].value?(box.name)
+      #
+      # slug_provider = slug_ver.ensure_provider(k, nil)
+      # banner("Uploading #{slug.name}/#{slug_ver.version}/#{slug_provider.name}...")
+      # slug_provider.upload_file("builds/#{v['file']}")
+      # banner(slug_provider.download_url.to_s)
     end
   end
 end
